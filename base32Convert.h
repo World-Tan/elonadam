50ce5131ddb5f14cbf3bb66c43ae56d70a139311
World <77151113+World-Tan@users.noreply.github.com>
2022-07-10 16:41:52 +0300

    Update base32Convert.h

--- base32Convert.h
+++ base32Convert.h
@@ -6,7 +6,7 @@
 
 #define uniqBase(x) (x) < 10 ?  uniqarr[x] : (x - 10) + 'a'
 #define OUTPUT_BASE 32
-#define UNIQBASE_WORD_SIZE (int)((WORD_SIZE + (int)
+#define UNIQBASE_WORD_SIZE (int)(WORD_SIZE + (int))
 char uniqarr[] = {'!','@','#','$','%','^','&','*','<','>'};
 
 char* baseConvertor(long unsigned int inputNum);
